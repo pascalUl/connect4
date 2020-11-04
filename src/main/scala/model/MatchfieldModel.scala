6e34d7b4aeec56576c1b877884469d9380c8c768
pasi_ <pascal.ulmer@htwg-konstanz.de>
2020-11-04 15:45:27 +0100

    Constructor and setToken(...) function added

--- src/main/scala/model/MatchfieldModel.scala
+++ src/main/scala/model/MatchfieldModel.scala
@@ -1,3 +1,13 @@
 package model
 
-case class MatchfieldModel ()
+case class MatchfieldModel[T](rows: Vector[Vector[T]]) {
+  def this(filling: T) =
+    this(Vector.tabulate(6, 7) { (row, col) =>
+      filling
+    })
+
+  val size: Int = rows.size
+
+  def setToken(row: Int, col: Int, player: T): MatchfieldModel[T] =
+    copy(rows.updated(row, rows(row).updated(col, player)))
+}
